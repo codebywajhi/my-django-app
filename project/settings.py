3b484a84d7d305fd84149d6fae1f310c45f2ecc1
wajhi <mrwajhiqureshi@gmail.com>
2025-01-01 16:44:19 +0500

    Trigger redeployment

--- project/settings.py
+++ project/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_URL = '/static/'
+
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
