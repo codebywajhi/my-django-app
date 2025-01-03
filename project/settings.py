ee1c0c0eb0555a4df06cc9b684c59e04df92c133
Muhammad <ashariqbal947@gmail.com>
2025-01-04 03:14:28 +0500

    chage in wsgi

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = [
     'my-django-8rvwfrmpx-wajhis-projects.vercel.app',
     'localhost',
     '127.0.0.1',
-    '89.116.50.221'
+    '89.116.50.221',
+    'bot-scrapper.marketxtractor.ai'
 ]
 
 # Application definition
--- project/wsgi.py
+++ project/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Add the virtual environment to the sys.path
+# Update this path to your virtual environment location
+activate_this = '/var/www/projects/django/my-django-app/venv/bin/activate_this.py'
+exec(open(activate_this).read(), dict(__file__=activate_this))
+
+# Add your project to the sys.path
+sys.path.append('/var/www/projects/django/my-django-app')
+
+# Set the Django settings module environment variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
